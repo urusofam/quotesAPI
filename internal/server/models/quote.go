fc9ddc6b42710f2315f906a72fd4c470d439f6ee
urusofam <urusofam@gmail.com>
2025-05-30 19:50:26 +0300

    del quoteRequest and change id string to int

--- internal/server/models/quote.go
+++ internal/server/models/quote.go
@@ -1,12 +1,7 @@
 package models
 
 type Quote struct {
-	ID     string `json:"id"`
-	Author string `json:"author"`
-	Quote  string `json:"quote"`
-}
-
-type QuoteRequest struct {
+	ID     int    `json:"id"`
 	Author string `json:"author"`
 	Quote  string `json:"quote"`
 }
