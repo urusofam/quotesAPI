70b16f26c253ed5fd39c17c9d77b824090bc304e
urusofam <urusofam@gmail.com>
2025-05-30 16:40:02 +0300

    add router

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
 
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/quotes", func(w http.ResponseWriter, r *http.Request) {}).Methods("POST")
+	router.HandleFunc("/quotes", func(w http.ResponseWriter, r *http.Request) {}).Methods("GET")
+	router.HandleFunc("/quotes/random", func(w http.ResponseWriter, r *http.Request) {}).Methods("GET")
+	router.HandleFunc("/quotes/{id}", func(w http.ResponseWriter, r *http.Request) {}).Methods("DELETE")
+
+	if err := http.ListenAndServe(":8080", router); err != nil {
+		log.Fatal(err)
+	}
 }
