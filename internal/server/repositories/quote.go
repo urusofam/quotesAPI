626e676cab83a4ae72eb6af885ea893df7dbd231
urusofam <urusofam@gmail.com>
2025-05-30 16:52:44 +0300

    add quote repository

---
+++ internal/server/repositories/quote.go
@@ -0,0 +1,48 @@
+package repositories
+
+import (
+	"errors"
+	"github.com/urusofam/quotesAPI/internal/server/models"
+)
+
+type QuoteRepository interface {
+	AddQuote(quote models.Quote)
+	GetAllQuotes() []models.Quote
+	GetQuoteByAuthor(author string) (models.Quote, error)
+	DeleteQuoteById(ID string) error
+}
+
+type quoteRepository struct {
+	quotes []models.Quote
+}
+
+func NewQuoteRepository() QuoteRepository {
+	return &quoteRepository{quotes: make([]models.Quote, 0)}
+}
+
+func (r *quoteRepository) AddQuote(quote models.Quote) {
+	r.quotes = append(r.quotes, quote)
+}
+
+func (r *quoteRepository) GetAllQuotes() []models.Quote {
+	return r.quotes
+}
+
+func (r *quoteRepository) GetQuoteByAuthor(author string) (models.Quote, error) {
+	for _, quote := range r.quotes {
+		if quote.Author == author {
+			return quote, nil
+		}
+	}
+	return models.Quote{}, errors.New("not found")
+}
+
+func (r *quoteRepository) DeleteQuoteById(ID string) error {
+	for i, quote := range r.quotes {
+		if quote.ID == ID {
+			r.quotes = append(r.quotes[:i], r.quotes[i+1:]...)
+			return nil
+		}
+	}
+	return errors.New("not found")
+}
